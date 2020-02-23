c0031e5a174fb437eda9eb970165a00d3bbe8a1c
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-23 08:45:26 -1000

    created listview

--- app/src/main/java/com/myappcompany/russell/memorableplaces/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/memorableplaces/MainActivity.java
@@ -2,6 +2,10 @@ package com.myappcompany.russell.memorableplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayList<String> places = new ArrayList<String>();
+
+        places.add("Add a new place...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
