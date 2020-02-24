e4da57171b374dcd5ad9701c1a593df60ca10259
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-24 08:58:44 -1000

    finding current address and setting it to marker title

--- app/src/main/java/com/myappcompany/russell/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/myappcompany/russell/memorableplaces/MapsActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -20,6 +22,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
@@ -105,6 +110,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapLongClick(LatLng latLng) {
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Your new Memorable Place"));
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        String address = "";
+
+        try {
+            List<Address> listAddresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude,1);
+            if(listAddresses != null && listAddresses.size() > 0){
+                if(listAddresses.get(0).getThoroughfare() != null){
+                    if(listAddresses.get(0).getSubThoroughfare() != null){
+                        address += listAddresses.get(0).getSubThoroughfare() + " ";
+                    }
+
+                    address += listAddresses.get(0).getThoroughfare();
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
     }
 }
